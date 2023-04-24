1ab5b7975c6f138f2c4add73a8b0c0a764350e76
up202004724 <up202004724@fe.up.pt>
2023-04-24 23:22:59 +0100

    Test

--- src/main/pt/up/fe/comp2023/OllirExpressionGenerator.java
+++ src/main/pt/up/fe/comp2023/OllirExpressionGenerator.java
@@ -162,7 +162,7 @@ public class OllirExpressionGenerator extends PreorderJmmVisitor<String, OllirTo
 
             // add preceding operation
             if (preCode != "") preCode += "\n";
-            preCode += s + tempVar + " :=." + leftOllirTools.getOpType() + " " + leftOllirTools.getCode() + ";";
+            preCode += s + tempVar + " :=." + leftOllirTools.getOpType() + " " + leftOllirTools.getCode();
 
             leftCode = tempVar;
         }
@@ -173,7 +173,7 @@ public class OllirExpressionGenerator extends PreorderJmmVisitor<String, OllirTo
 
             // add preceding operation
             if (preCode != "") preCode += "\n";
-            preCode += s + tempVar + " :=." + rightOllirTools.getOpType() + " " + rightOllirTools.getCode() + ";";
+            preCode += s + tempVar + " :=." + rightOllirTools.getOpType() + " " + rightOllirTools.getCode();
 
             rightCode = tempVar;
         }
