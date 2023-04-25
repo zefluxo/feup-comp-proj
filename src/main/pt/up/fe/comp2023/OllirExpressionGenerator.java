e51736f96a867c68a87eccecbae141502c725e3f
up202004724 <up202004724@fe.up.pt>
2023-04-25 12:09:30 +0100

    test7

--- src/main/pt/up/fe/comp2023/OllirExpressionGenerator.java
+++ src/main/pt/up/fe/comp2023/OllirExpressionGenerator.java
@@ -100,7 +100,7 @@ public class OllirExpressionGenerator extends PreorderJmmVisitor<String, OllirTo
         }
 
         // static method call or virtual method call
-        if (!objName.contains(".") && !objName.equals("this")){
+        if (!objName.contains("." + this.symbolTable.getClassName()) && !objName.equals("this")){
             code += "invokestatic(" + objName + ", \"" + jmmNode.get("methodName") + "\"" + arguments + ").V";
         } else {
             String returnType = OllirTools.getOllirType(this.symbolTable.getReturnType(jmmNode.get("methodName")).getName());
